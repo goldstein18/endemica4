2db09106e2ec5f319e378b9cd6cfa43be497dd4a
M <76853962+goldstein18@users.noreply.github.com>
2024-04-26 13:25:56 -0600

    Update jquery.meanmenu.js

--- assets/js/jquery.meanmenu.js
+++ assets/js/jquery.meanmenu.js
@@ -32,7 +32,7 @@
 						meanMenucontainer: '.mobile-menu-area .container', // Choose where meanmenu will be placed within the HTML
 						meanMenuClose: "X", // single character you want to represent the close menu button
 						meanMenuCloseSize: "18px", // set font size of close button
-						meanMenuOpen: "<span /><span /><span />", // text/markup you want when menu is closed
+						meanMenuOpen: "<span></span><span></span><span></span>", // text/markup you want when menu is closed
 						meanRevealPosition: "right", // left right or center positions
 						meanRevealPositionDistance: "0", // Tweak the position of the menu
 						meanRevealColour: "", // override CSS colours for the reveal background
